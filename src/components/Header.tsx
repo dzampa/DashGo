dc19d581f41659f4ac99e724f6708a40fcdf078e
Diego Zamparo <diego_zamparo@hotmail.com>
2022-05-03 09:49:04 -0300

    header finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri"
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from "react-icons/ri"
 
 export function Header() {
     return (
@@ -14,7 +14,7 @@ export function Header() {
             align="center"
         >
             <Text
-                fontStyle="3xl"
+                fontSize="3xl"
                 fontWeight="bold"
                 letterSpacing="tight"
                 w="64"
@@ -45,6 +45,34 @@ export function Header() {
                     _placeholder={{ color: 'gray.400' }}
                 />
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack
+                    spacing="4"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+                <Flex
+                    align="center"
+                >
+                    <Box mr="4" textAlign="right">
+                        <Text>Diego Zamparo</Text>
+                        <Text color="gray.300" fontSize="small">
+                            dzampar@gmail.com
+                        </Text>
+                    </Box>
+                    <Avatar size="md" name="Diego Zamparo" src="https://avatars.githubusercontent.com/u/51325668?s=40&v=4" />
+                </Flex>
             </Flex>
         </Flex>
     )
