cc7e0d5023741d5df6d11bf954593fc5636e75fd
Diego Zamparo <diego_zamparo@hotmail.com>
2022-05-19 07:28:00 -0300

    applying responsibility

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,22 @@
 import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex
             align="center"
         >
-            <Box mr="4" textAlign="right">
-                <Text>Diego Zamparo</Text>
-                <Text color="gray.300" fontSize="small">
-                    dzampar@gmail.com
-                </Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Diego Zamparo</Text>
+                    <Text color="gray.300" fontSize="small">
+                        dzampar@gmail.com
+                    </Text>
+                </Box>
+            )}
             <Avatar size="md" name="Diego Zamparo" src="https://avatars.githubusercontent.com/u/51325668?s=40&v=4" />
         </Flex>
     );
