7491aa39861639ca60ae2fec77114a827b6e858b
Diego Zamparo <diego_zamparo@hotmail.com>
2022-05-23 07:36:59 -0300

    ref in search

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,7 +1,10 @@
 import { Flex, HStack, Icon, Input } from "@chakra-ui/react";
+import { useRef } from "react";
 import { RiNotificationLine, RiSearchLine, RiUserAddLine } from "react-icons/ri";
 
 export function SearchBox() {
+    const searchInputRef = useRef<HTMLInputElement>(null)
+
     return (
         <Flex
             as="label"
@@ -23,6 +26,7 @@ export function SearchBox() {
                 mr="4"
                 placeholder="Buscar na plataforma"
                 _placeholder={{ color: 'gray.400' }}
+                ref={searchInputRef}
             />
             <Icon as={RiSearchLine} fontSize="20" />
         </Flex>
