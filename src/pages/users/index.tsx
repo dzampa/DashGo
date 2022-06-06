7d1462f9977bea7ca0253f94499cdf891d2de7a0
Diego Zamparo da Silva <diego.silva@grupoboticario.com.br>
2022-06-06 17:57:24 -0300

    added react query on ssr

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,18 +17,21 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { GetServerSideProps } from "next/types";
 import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { api } from "../../services/api";
-import { useUsers } from "../../services/hooks/useUsers";
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
 import { queryClient } from "../../services/queryClient";
 
 export default function UserList() {
   const [page, setPage] = useState(1);
-  const { data, isLoading, isFetching, error } = useUsers(page);
+  const { data, isLoading, isFetching, error } = useUsers(page, {
+    initialData: users,
+  });
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -143,3 +146,12 @@ export default function UserList() {
     </Box>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1);
+  return {
+    props: {
+      users,
+    },
+  };
+};
